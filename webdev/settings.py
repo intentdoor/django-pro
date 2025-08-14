f53018b916bf39b8c86cfa95d12bb431656d06fd
DEV-T-2025 <arthuramerico33@gmail.com>
2025-08-14 16:57:06 -0300

    arrumando o static

--- webdev/settings.py
+++ webdev/settings.py
@@ -120,6 +120,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join( BASE_DIR , "static") 
+STATICFILES_DIR = [
+    BASE_DIR / "mycontacts/static"
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
